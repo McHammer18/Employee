2030371e096c65d20ff43f29a820a9fa8dd226f6
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-11-01 14:33:51 -0600

    classes created
    Employee and departement classes have their properties and string methods added to them. also entity class declared along with connection to the department class

--- src/main/java/dmacc/beans/Department.java
+++ src/main/java/dmacc/beans/Department.java
@@ -1,5 +1,50 @@
 package dmacc.beans;
 
-public class Department {
+import javax.persistence.Embeddable;
 
+@Embeddable
+public class Department {
+	private String type;	
+	private String location;
+	
+	//Constructors
+	public Department() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Department(String type, String location) {
+		super();
+		this.type = type;
+		this.location = location;
+	}
+	
+	//getters and setters
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [Type = " + type + ", Location = " + location;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/dmacc/beans/Employee.java
+++ src/main/java/dmacc/beans/Employee.java
@@ -1,5 +1,85 @@
 package dmacc.beans;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Entity
 public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String lastName;
+	private String firstName;
+	private String phone;
+	@Autowired
+	private Department department;
+	
+	//Constructors
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Employee(String lastName, String firstName) {
+		super();
+		this.lastName = lastName;
+		this.firstName = firstName;
+	}
+	public Employee(String lastName, String firstName, String phone) {
+		super();
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.phone = phone;
+	}
+	public Employee(long id, String lastName, String firstName, String phone) {
+		super();
+		this.id = id;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.phone = phone;
+	}
+	
+	//getters and setters
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	@Override
+	public String toString() {
+		return "Employee [Id = " + id + ", Last Name = " + lastName + ", First Name = " 
+				+ firstName + ", Phone = " + phone + ", Department = " + department;
+	}
+	
+	
+	
 
 }
